64cb380bff215de858d05f611d1ee3ea999090f9
bzquatre <bzquatre@gmail.com>
2023-05-26 01:53:44 +0100

    Gig change

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # biparti_transvarsal_minimal
-ghrphe biparti et transvarsal minimal
+The algorithm works by first finding a maximum matching in the bipartite graph, which is a set of edges that pairs as many nodes as possible without sharing any common nodes. Then, the maximum matching is converted into a vertex cover, which is a set of nodes that covers all the edges in the graph. Since the vertex cover alternates between the sets X and Y, it represents the minimum number of nodes required to cover all the edges in the graph.
+
+## Environment
+
+1. Install virtualenv:
+```
+pip install virtualenv
+```
+2. Create a virtual environment:
+```
+python -m  virtualenv env
+```
+
+3. Activate the virtual environment:
+- On Windows:
+  ```
+  env\Scripts\activate
+  ```
+- On macOS and Linux:
+  ```
+  source env/bin/activate
+  ```
+## Installation
+
+Install the required packages:
+```
+pip install -r requirements.txt
+```
+## Execution
+
+Run the main script:
+```
+python main.py
+```
+## Build
+
+Build the project:
+```
+python setup.py
+```
+## Installation File
+Click [here](https://drive.google.com/drive/folders/15tkfofVFKI3tjrjyWhQUSh4uVjxwZ0-R?usp=share_link) to download the installation file.
+
+
+
+![Example Image](./TransvarsalMinimum.png)
--- main.py
+++ main.py
@@ -1,47 +1,45 @@
-from PyQt5.QtWidgets import QApplication,QWidget,QLabel,QLineEdit,QPushButton,QTableWidget,QHBoxLayout,QVBoxLayout,QMessageBox
-from PyQt5.QtGui import QIntValidator
-import sys
+from PyQt5.QtWidgets import QApplication,QFileDialog, QTableWidgetItem,QWidget,QLabel,QLineEdit,QPushButton,QTableWidget,QHBoxLayout,QVBoxLayout,QMessageBox
+from PyQt5.QtGui import QIntValidator,QIcon
+import sys,os,csv
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
-def style():
-    return"""
-    
 
-            """
 class Windo(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("USTHB >> youcef >> ghrphe biparti et transvarsal minimal ")
+        self.setWindowIcon(QIcon('Icon.ico'))
+        self.setWindowTitle("ghrphe biparti et transvarsal minimal ")
         self.setGeometry(200,100,1024,600)
-        self.colm=QLineEdit()
-        self.colm.returnPressed.connect(self.addcolm)
-        self.line=QLineEdit()
-        self.colm.setValidator(QIntValidator(0,10000,self))
-        self.line.returnPressed.connect(self.addline)
+        self.columns,self.lines=QLineEdit(),QLineEdit()
+        self.columns.setValidator(QIntValidator(0,100))
+        self.lines.setValidator(QIntValidator(0,100,self))
+        self.lines.textChanged.connect(self.addLines)
+        self.columns.textChanged.connect(self.addColumns)
         self.table=QTableWidget()
-        self.table.horizontalHeader().setStyleSheet("background-color:#ffffff;font-size: 25px;color:#00AEFF;border-radius:0px;")
-        self.table.verticalHeader().setStyleSheet("background-color:#ffffff;font-size: 25px;color:#00AEFF;border-radius:0px;")
-        self.line.setValidator(QIntValidator(0,10000,self))
-        self.btnghraph1=QPushButton(text="Ghraphe Biparti")
-        self.btnghraph1.clicked.connect(self.listable)
-        self.btntranv=QPushButton(text= "Transvarsal Minimum")
-        self.btntranv.clicked.connect(self.btn2)
-        self.btnghraph2=QPushButton(text="Ghraphe de Transvarsal Minimum")
-        self.btnghraph2.setStyleSheet('padding: 15px 25px;')
-        self.btnghraph2.clicked.connect(self.btn3)
-        self.setLayout(self.layout())
+        self.table.itemChanged.connect(self.disavleButtons)
+        self.create_bipartite_graph=QPushButton(text="Ghraphe Biparti")
+        self.create_bipartite_graph.clicked.connect(self.bipartiteGraph)
+        self.transvarsal_minimum=QPushButton(text= "Transvarsal Minimum")
+        self.import_csv=QPushButton(text= "Import CSV")
+        self.import_csv.clicked.connect(self.importCsv)
+        self.transvarsal_minimum.clicked.connect(self.calculateTransvarsalMinimum)
+        self.ghraphe_Transvarsal_minimum=QPushButton(text="Ghraphe de Transvarsal Minimum")
+        self.ghraphe_Transvarsal_minimum.clicked.connect(self.drawGhrapheTransvarsalMinimum)
+        self.setLayout()
         self.show()
-        self.showMaximized()
-    def btn2(self):
+    def disavleButtons(self):
+        self.transvarsal_minimum.setDisabled(True)
+        self.ghraphe_Transvarsal_minimum.setDisabled(True)
+    def calculateTransvarsalMinimum(self):
         try:
             matching = nx.bipartite.maximum_matching(self.B)
             vertex_cover = nx.bipartite.to_vertex_cover(self.B, matching)
         except:
             QMessageBox.information(self,'ERROR ',"le ghraphe net pas bipartite")
         else:
-            QMessageBox.information(self,'Trenversal minimal ',f"T = {','.join(list(vertex_cover))}       Card(T) ={len(list(vertex_cover))}")
-    def btn3(self):
+            QMessageBox.information(None,'Trenversal minimal ',f"T = {','.join(list(vertex_cover))}       Card(T) ={len(list(vertex_cover))}")
+    def drawGhrapheTransvarsalMinimum(self):
          try:
             matching = nx.bipartite.maximum_matching(self.B)
             vertex_cover = nx.bipartite.to_vertex_cover(self.B, matching)
@@ -58,68 +56,61 @@ class Windo(QWidget):
             nx.draw(self.B,self.pos, node_color=color_map, with_labels=False)
             nx.draw_networkx_labels(self.B, self.pos1)
             plt.show()
-    def separ(self,s,a):
-        s=s
-        x=[]
-        y=[]
-        x.append(s[0])  
-        i=0
-        J=0
-        while len(x)+len(y)<len(s):
-            print(i) 
-            if s[i] in x :  
-                for j in a:
-                    if j[0]==s[i] :
-                        if j[1] not in y:
-                            y.append(j[1])
-                    if j[1]==s[i] :
-                        if j[0] not in y:
-                            y.append(j[0])
-            elif s[i] in y :  
-                for j in a:
-                    if j[0]==s[i] :
-                        if j[1] not in x :
-                            x.append(j[1])
-                    elif j[1]==s[i] :
-                        if j[0] not in x:
-                            x.append(j[0])
-            print(i)
-            if i==len(s)-1:
-                i=0
-                J+=1
-                if J==len(s)-1:
-                    return QMessageBox.information(self,'ERROR ',"le ghraphe net pas bipartite")
-            else:
-                i+=1  
-        return x,y
-    def listable(self):
-        
+    def bipartiteGraph(self):
+        def separ(s,a):
+            s=s
+            x=[]
+            y=[]
+            x.append(s[0])  
+            i=0
+            J=0
+            while len(x)+len(y)<len(s):
+               
+                if s[i] in x :  
+                    for j in a:
+                        if j[0]==s[i] :
+                            if j[1] not in y:
+                                y.append(j[1])
+                        if j[1]==s[i] :
+                            if j[0] not in y:
+                                y.append(j[0])
+                elif s[i] in y :  
+                    for j in a:
+                        if j[0]==s[i] :
+                            if j[1] not in x :
+                                x.append(j[1])
+                        elif j[1]==s[i] :
+                            if j[0] not in x:
+                                x.append(j[0])
+              
+                if i==len(s)-1:
+                    i=0
+                    J+=1
+                    if J==len(s)-1:
+                        return QMessageBox.information(self,'ERROR ',"le ghraphe net pas bipartite")
+                else:
+                    i+=1  
+            return x,y
         l=[]
-        for i in range(self.table.rowCount()-1):
+        for i in range(1,self.table.rowCount()):
             try:
-                l.append(self.table.item(i+1,0).text())
+                l.append(self.table.item(i,0).text())
+            except:
+                return QMessageBox.information(self,'ERROR ',"size tout les sommet")
+        for i in range(1,self.table.columnCount()):
+            try:
+                l.append(self.table.item(0,i).text())
             except:
                 return QMessageBox.information(self,'ERROR ',"size tout les sommet")
         z=[]
-        for i in range(self.table.columnCount()-1):
-            k=[]   
-            for j in range(self.table.rowCount()-1): 
-                try:
-                    m=self.table.item(j+1,i+1).text()
-                except:
-                    pass
-                else:
-                    if m=='1':
-                        k.append(self.table.item(j+1,0).text())  
-                    elif m=='' or m=='0':
-                        pass
-                    else:
-                        return QMessageBox.information(self,'ERROR ',"la matrice net pas insidence")
-            if len(k)!=2:
-                return QMessageBox.information(self,'ERROR ',"la matrice net pas insidence")
-            z.append((k[0],k[1],{'weight': 1}))
+        for i in range(1,self.table.columnCount()):
+            for j in range(1,self.table.rowCount()): 
+                if not self.table.item(j,i) :
+                    return QMessageBox.information(self,'ERROR ',"size tout les sommet")
+                if self.table.item(j,i).text()=='1':
+                    z.append((self.table.item(j,0).text(),self.table.item(0,i).text(),{'weight': 1}))
         try:
-            x,y=self.separ(l,z)
+            x,y=separ(l,z)
         except:
             pass
         else:
@@ -137,26 +128,46 @@ class Windo(QWidget):
             plt.show()
             self.pos1=pos
             self.B=B
-    def addcolm(self):
-        self.table.setColumnCount(int(self.colm.text())+1)
-    def addline(self):
-        self.table.setRowCount(int(self.line.text())+1)
-        self.showMaximized()
-    def layout(self):
+            self.transvarsal_minimum.setDisabled(False)
+            self.ghraphe_Transvarsal_minimum.setDisabled(False)
+    def importCsv(self):
+        home_directory = os.getenv('HOME')
+        fname, _ = QFileDialog.getOpenFileName(self, 'Open CSV File', home_directory, 'CSV files (*.csv)')
+        if fname!='':
+            with open(fname, 'r') as file:
+                reader = csv.reader(file)
+                data = list(reader)
+            self.lines.setText(str(len(data)-1))
+            self.columns.setText(str(len(data[0])-1))
+            for row in range(len(data)):
+                for column in range(len(data[row])):
+                    self.table.setItem(row, column, QTableWidgetItem(data[row][column]))
+    def addColumns(self):
+        try:
+            self.table.setColumnCount(int(self.columns.text())+1)
+        except:
+            pass
+    def addLines(self): 
+        try:
+            self.table.setRowCount(int(self.lines.text())+1)
+        except:
+            pass
+    def setLayout(self):
         v=QVBoxLayout()
         h=QHBoxLayout()
         h.addWidget(QLabel('X :'))
-        h.addWidget(self.colm)
+        h.addWidget(self.columns)
         h.addWidget(QLabel('Y :'))
-        h.addWidget(self.line)
+        h.addWidget(self.lines)
+        h.addWidget(self.import_csv)
         h1=QHBoxLayout()
-        h1.addWidget(self.btnghraph1)
-        h1.addWidget(self.btntranv)
-        h1.addWidget(self.btnghraph2)
+        h1.addWidget(self.create_bipartite_graph)
+        h1.addWidget(self.transvarsal_minimum)
+        h1.addWidget(self.ghraphe_Transvarsal_minimum)
         v.addLayout(h)
         v.addWidget(self.table)
         v.addLayout(h1)
-        return v
+        return super().setLayout(v)
 if __name__=="__main__":
     app=QApplication(sys.argv)
     app.setStyleSheet(open("style.qss","r").read())
